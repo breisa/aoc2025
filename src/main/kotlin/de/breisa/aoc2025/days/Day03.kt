a61c9dd30c6624670e970102a231423f9a2910be
breisa <breisa@posteo.me>
2025-12-03 09:39:04 +0100

    add solution for day 3

---
+++ src/main/kotlin/de/breisa/aoc2025/days/Day03.kt
@@ -0,0 +1,63 @@
+package de.breisa.aoc2025.days
+
+import de.breisa.aoc2025.getResourceAsText
+
+private val EXAMPLE = """
+    987654321111111
+    811111111111119
+    234234234234278
+    818181911112111
+""".trimIndent()
+
+class Day03: Day<List<List<Long>>, Long> {
+    override fun solveFirstPart(input: List<List<Long>>): Long {
+        return input.sumOf { bank ->
+            var left = 0L
+            var right = 0L
+            var maxValue = 0L
+            for (digit in bank) {
+                val newRight = left * 10 + digit
+                val shifted = right * 10 + digit
+                if (shifted > maxValue && shifted > newRight) {
+                    left = right
+                    right = digit
+                } else if (newRight > maxValue) {
+                    right = digit
+                }
+                maxValue = left * 10 + right
+            }
+            maxValue
+        }
+    }
+
+    override fun solveSecondPart(input: List<List<Long>>): Long {
+        // generalized algorithm: add digit to the end and drop the first digit that is lower than the next one
+        val BATTERY_COUNT = 12
+        return input.sumOf { bank ->
+            val joltages = bank.take(BATTERY_COUNT).toMutableList()
+            bank.drop(BATTERY_COUNT).forEach { nextDigit ->
+                joltages.addLast(nextDigit)
+                val dropIndex = (0..<BATTERY_COUNT).firstOrNull { index ->
+                    joltages[index] < joltages[index + 1]
+                } ?: 12
+                joltages.removeAt(dropIndex)
+            }
+            joltages.joinToString("").toLong()
+        }
+    }
+
+    override fun parseInput(data: String): List<List<Long>> = data
+        .lines()
+        .map { line ->
+            line.map { digit ->
+                digit.digitToInt().toLong()
+            }
+        }
+}
+
+fun main() {
+    Day03().solve(
+        exampleInput = EXAMPLE,
+        puzzleInput = getResourceAsText("/puzzles/03/input.txt")
+    )
+}
